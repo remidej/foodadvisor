eaa25a20f345f105dbc5532b4d68bdd99845a409
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2021-02-25 18:03:04 +0100

    Reset demo state

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -1,9 +1,5 @@
-module.exports = ({ env }) => ({
-  // All plugins config go here
-  sentry: {
-    dsn: env('SENTRY_DSN'),
-    init: {
-      release: env('SENTRY_RELEASE'),
-    }
+module.exports = ({ env }) => {
+  return {
+    // All plugins config go here
   }
-});
+};
