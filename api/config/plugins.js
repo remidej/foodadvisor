d76bbb9e1f044d30444679b836ad8a2279f0bcff
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2021-02-25 18:42:00 +0100

    Fixes FOODADVISOR-API-2

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -1,5 +1,11 @@
 module.exports = ({ env }) => {
   return {
     // All plugins config go here
+    sentry: {
+      dsn: env('SENTRY_DSN'),
+      init: {
+        release: env('SENTRY_RELEASE'),
+      }
+    }
   }
 };
